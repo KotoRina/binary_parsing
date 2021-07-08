8b08473e517297dac418ffce0e98670227942a5f
Katerina <64101388+KotoRina@users.noreply.github.com>
2021-07-09 02:01:55 +0300

    edit setting, add ref name

--- binary_parsing/binary_parsing/setting.h
+++ binary_parsing/binary_parsing/setting.h
@@ -3,13 +3,15 @@
 
 #include <iostream>
 #include <vector>
+#include <fstream>
+#include <list>
 
-static std::string	NameFile = "input.txt";
+static std::string	pathFile = "input.txt";
 
-struct InputData
+struct Pakege
 {
-	std::string					name;
-	long						length;
+	std::string					name = "";
+	long						length = 0;
 	std::vector<std::string>	data;
 };
 
